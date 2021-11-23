2ed80ca6dc1f266b665b9163b2662f7d82ceefff
Karolina Guzik <karolinaguzik@Karolinas-MacBook-Air.local>
2021-11-23 16:16:38 +0100

    Updates

---
+++ precache-manifest.bf165f864378214746a240019cdeecaa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00a4eace585074bcd65ef625ae94307",
+    "url": "/portfolio-page/index.html"
+  },
+  {
+    "revision": "534c92f6521d0db43332",
+    "url": "/portfolio-page/static/css/2.25249b3f.chunk.css"
+  },
+  {
+    "revision": "ef970a10c3d611379ff7",
+    "url": "/portfolio-page/static/css/main.82ff2aba.chunk.css"
+  },
+  {
+    "revision": "534c92f6521d0db43332",
+    "url": "/portfolio-page/static/js/2.1f99ff21.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio-page/static/js/2.1f99ff21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef970a10c3d611379ff7",
+    "url": "/portfolio-page/static/js/main.38d0488a.chunk.js"
+  },
+  {
+    "revision": "00d16d7d1322f20f3dae",
+    "url": "/portfolio-page/static/js/runtime-main.7c152ff0.js"
+  },
+  {
+    "revision": "66b223ea32f8ddd4f7324f3d3cb2cb6e",
+    "url": "/portfolio-page/static/media/apdiet.66b223ea.png"
+  },
+  {
+    "revision": "0dc3d41e959330da19f8d9541a34d741",
+    "url": "/portfolio-page/static/media/biznestoq.0dc3d41e.png"
+  },
+  {
+    "revision": "971354ea3a39f5cfb312264dd588d196",
+    "url": "/portfolio-page/static/media/consulting.971354ea.png"
+  },
+  {
+    "revision": "52603eab3eaa42f431f2e85f8d963bb2",
+    "url": "/portfolio-page/static/media/corpussanum.52603eab.png"
+  },
+  {
+    "revision": "3a65c4bbcfa09ccdebfcb71fd10dba6c",
+    "url": "/portfolio-page/static/media/jansbud.3a65c4bb.png"
+  },
+  {
+    "revision": "186de669e2cb56436d3a270c68f48ec5",
+    "url": "/portfolio-page/static/media/ktory-kosz.186de669.png"
+  },
+  {
+    "revision": "1322d4862048bfaf7219ed77220ab87d",
+    "url": "/portfolio-page/static/media/me-blue.1322d486.png"
+  },
+  {
+    "revision": "f617d4e9e5b6b7cc679cb3f484c8d6a3",
+    "url": "/portfolio-page/static/media/moena.f617d4e9.png"
+  },
+  {
+    "revision": "f817f1f19cd93b8544582ad252d13341",
+    "url": "/portfolio-page/static/media/movie-quotes-quiz.f817f1f1.png"
+  },
+  {
+    "revision": "fa9d6ffb09d326f67201e463e6609acc",
+    "url": "/portfolio-page/static/media/pozytywnie.fa9d6ffb.png"
+  },
+  {
+    "revision": "42d9915402ce072aecb9becde5e816a4",
+    "url": "/portfolio-page/static/media/reveil.42d99154.png"
+  },
+  {
+    "revision": "35e13fc7977b6930e5714c9354f60c16",
+    "url": "/portfolio-page/static/media/tazz.35e13fc7.png"
+  },
+  {
+    "revision": "3cd186070ae69ecc950b7095d63ca331",
+    "url": "/portfolio-page/static/media/turystyczny.3cd18607.png"
+  },
+  {
+    "revision": "61001ca85eb70a12309ef9da24a8a489",
+    "url": "/portfolio-page/static/media/wdziecznopis.61001ca8.png"
+  },
+  {
+    "revision": "62692591f147210ec1e7316d045b1d51",
+    "url": "/portfolio-page/static/media/weather-finder.62692591.png"
+  }
+]);
