8ef7bb05f5ed3ad3df054dffc34aa1aaf0a31224
Karolina Guzik <karolinaguzik@Karolinas-MacBook-Air.local>
2021-11-23 15:58:37 +0100

    update images, small changes in styles and json file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <FullPage controls={false}>
         <Slide>
-          <Header title="Karolina Guzik" subtitle="Front-End Developer" text="I have 3 years of experience working as a Front-End Developer / Web Developer. For the last year I worked as a freelancer. I worked mainly with WordPress, HTML5, Sass, Bootstrap and JavaScript technologies."/>
+          <Header title="Karolina Guzik" subtitle="Front-End Developer" text="I have 3 years of experience working as a Front-End Developer / Web Developer. I worked mainly with WordPress, HTML5, Sass, Bootstrap and JavaScript technologies. For the last year, I have been developing websites for customers as a Freelancer."/>
         </Slide>
         <Slide>
           <Slider title="Selected projects"/>
@@ -22,3 +22,4 @@ function App() {
 }
 
 export default App;
+
